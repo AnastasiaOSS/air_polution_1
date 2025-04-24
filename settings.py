7d6b5c763ebe23aa0fededffe8a84a75d3af0784
AnastasiaOSS <anastasiapkspb@outlook.com>
2025-04-25 01:13:17 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,16 @@
+MY_PALETTE = ['#5E63B6', '#A393EB', '#F5C7F7', '#B8F4EE', '#BAD9BC']
+
+
+GRAPH_TITLE_FONT_SIZE = 25
+GRAPH_TITLE_ALIGN = 0.5
+GRAPH_TITLE_WEIGHT = 500
+GRAPH_FONT_SIZE = 16
+
+PLOT_BACKGROUND = 'rgba(240, 240, 240, 1.0)'
+PAPER_BACKGROUND = 'rgba(255, 255, 255, 1.0)'
+
+DROPDOWN_STYLE = {'color': '#333333',
+                  'backgroundColor': 'rgba(240, 240, 240, 1.0)',
+                  'border': '1px solid rgba(230, 230, 230, 0.7)',
+                  'borderRadius': '5px',
+                  }
